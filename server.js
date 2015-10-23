f7ee1f8d332887d231fffdbb4cc52d24613c07c8
annieredmond1 <annieredmond1@gmail.com>
2015-10-22 19:03:05 -0700

    made request object and restyled

--- server.js
+++ server.js
@@ -17,9 +17,14 @@ app.get('/', function(req, res) {
   res.render("index");
 });
 
-app.get('/demoprofile', function(req, res) {
-  res.render("demoprofile");
+var requests = [
+	{completed: false, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." }, { completed: true, description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable." }, { completed: false, description: "Hello I'm a prayer request"}
+];
+
+app.get('/demo', function(req, res) {
+  res.render("user-show", {requests:requests, demo:true});
 });
+
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("express-heroku-starter is running on port 3000");
