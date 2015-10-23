fd85f68c909f656313e77b4f1ee83f7674bc7e9c
annieredmond1 <annieredmond1@gmail.com>
2015-10-22 17:08:08 -0700

    demo user can mark posts as answered

--- public/js/demoprof.js
+++ public/js/demoprof.js
@@ -10,17 +10,25 @@ $(document).ready(function(){
   	e.preventDefault();
   	var formdata = $('#new-request-input').val();
   	console.log("request is: " + formdata);
-  	var requestHtml = "<li>" + formdata + "<br><br><button class='answered'>Mark as answered</button><br><br><button class='delete'>Delete</button></li>";
+  	var requestHtml = "<li>" + formdata + "<br><br><input class='answered' type='button' value='Mark as answered'></input><br><br><input class='delete' type='button' value='Delete'></input></li>";
   	$(this)[0].reset();
   	$('.active').prepend(requestHtml);
   });
 
-  //When delete button is clicked
+  //When delete button is clicked remove post
   $('.requests').on('click', '.delete', function() {
   	console.log("delete button was clicked");
   	var deleteRequest = $(this).closest('li');
   	$(deleteRequest).remove();
+  });
 
+  //When mark as answered button is clicked remove post and append to answered list
+  $('.requests').on('click', '.answered', function() {
+  	console.log("answered button was clicked");
+  	var answerRequest = $(this).closest('li');
+  	var answerHtml = "<li>" + answerRequest.text() + "<br><br><input class='delete' type='button' value='Delete'></input></li>";
+  	$('.completed').prepend(answerHtml);
+  	$(answerRequest).remove();
   });
 
 
